bb398fdfa41f217e75f08a3f4a6b4380f9278ff8
muac404 <muac404@gmail.com>
2022-10-19 15:55:07 +0200

    Aggiunge script genera password

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,13 @@
 // CHIEDI IL NOME
+const firstName=prompt("Scrivi il tuo nome");
+
 // CHIEDI IL COGNOME
+const lastName=prompt("Scrivi il tuo cognome");
 // CHIEDI IL COLORE PREFERITO
-// CREO UNA VARIABILE PASSWORD CHE CONTIENE LE TRE INFO RICHIESTE + IL NUMERO 76
-// STAMPO LA PAGINA HTML
+const favouriteColor=prompt("Digita il tuo colore preferito");
+// CREO UNA VARIABILE PASSWORD CHE CONTIENE LE TRE INFO RICHIESTE + IL NUMERO 76ù
+const password=firstName+lastName+favouriteColor+"76"
+// STAMPO IN CONSOLE IL RISULTATO
+console.log(password);
+//STAMPO SU HTML LA PASSWORD
+document.getElementById('password-result').innerHTML=password;
